6ffc04ec27fa40622965df92204affb15c855080
Ivan Ivani <34777752+realivanivani@users.noreply.github.com>
2025-03-17 23:36:36 +0100

    Update apply_operator.sql

--- apply_operator.sql
+++ apply_operator.sql
@@ -1,4 +1,4 @@
--- The APPLY operator is used to join a table with a table-valued function, allowing row-by-row evaluation. 
+-- The APPLY operator is used to join a table with a table-valued function, allowing row-by-row evaluation. It can be either a CROSS APPLY or an OUTER APPLY. The OUTER APPLY is similar to a LEFT OUTER JOIN except that the right table expression will get executed once for each row in the left table.
 -- In this example, the OUTER APPLY operator is used to retrieve the last 5 transactions for each customer.
 
 -- Get the last 5 transactions for selected customers
